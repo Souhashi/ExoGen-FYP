ba62211c30d991c804a9e2f184213d4714e03c63
Andreani Soupashi <wf003668@live.reading.ac.uk>
2019-03-07 13:43:47 +0000

    Key object added

---
+++ Assets/Key/Scripts/KeyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBehaviour : MonoBehaviour {
+
+    public BoxCollider2D keyCollider;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            DeactivateGameObject();
+        }
+    }
+
+    void DeactivateGameObject()
+    {
+        keyCollider.enabled = false;
+        gameObject.SetActive(false);
+    }
+	void Start () {
+        keyCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	
+}
